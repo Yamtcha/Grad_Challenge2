a0387362cc416ddb730fcf1881292fffccef3e0c
Yamkela Venfolo <yamkela.venfolo@SAMAC137.local>
2018-01-30 10:59:45 +0200

    Santa_Naught_List completed

--- Santa_Naughty_List.js
+++ Santa_Naughty_List.js
@@ -1,12 +1,12 @@
-let santasList = ["Tom","Errol","Sam", "mistyMay","Peter","Jennifer", "macMerphy"];
-let children = ["Errol", "Peter", "jennifer", "mistymay", "MacMerphy"];
+let santasList = ["Tom","Errol","Sam","Peter","Jennifer"];
+let children = ["Errol", "Peter", "Jennifer"];
 
 function findChildren(santasList,children) {
     // filtering Santa list to see which children to not appear there. (case sensitive)
     var uniqueArray = children.filter(function(elem) {
         return santasList.includes(elem);
     }
-);
+    );
 //Sort the array into alphabetical order.
 uniqueArray.sort();
 console.log(uniqueArray); 
